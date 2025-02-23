d040443ddfcb94b1ba44500c04bb30121a792b8d
AnasRiadi <anas.usta122@gmail.com>
2025-02-23 23:33:56 +0000

    Applied inline CSS styling to React components

---
+++ Desktop/alx-fe-reactjs/alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const MainContent = () => {
+  return (
+    <main style={{ padding: "20px", backgroundColor: "#e6f7ff" }}>
+      <h2 style={{ color: "darkblue" }}>Welcome to My Website</h2>
+      <p style={{ fontSize: "16px" }}>Explore my favorite cities and learn more about them.</p>
+    </main>
+  );
+};
+
+export default MainContent;
